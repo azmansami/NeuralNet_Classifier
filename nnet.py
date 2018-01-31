eacda9151a05a84c6b6de8011459fcb1956eb9ef
Azman Sami <azmansami@gmail.com>
2018-01-30 22:36:14 -0800

    added graph for tensorbaord visualization

--- nnet.py
+++ nnet.py
@@ -26,8 +26,8 @@ mnist = mnist_data.read_data_sets("data", one_hot=True, reshape=False, validatio
 x=tf.placeholder(tf.float32, shape=[None,28,28,1],name="input")
 y_=tf.placeholder(tf.float32, shape=[None,10],name="y_actual")
 
-lr=tf.placeholder(tf.float32)
-pkeep=tf.placeholder(tf.float32)
+lr=tf.placeholder(tf.float32,name="LearningRate")
+pkeep=tf.placeholder(tf.float32,name="Regularization")
 # 5 layer neural net.
 # layer 1 = 200 node
 # layer 2 = 100 node
@@ -39,49 +39,66 @@ L2=100
 L3=60
 L4=30
 
-w1 = tf.Variable(tf.truncated_normal([784,L1],stddev=0.1))
-b1 = tf.Variable(tf.zeros([L1])/10)
-w2 = tf.Variable(tf.truncated_normal([L1,L2],stddev=0.1))
-b2 = tf.Variable(tf.zeros([L2])/10)
-w3 = tf.Variable(tf.truncated_normal([L2,L3],stddev=0.1))
-b3 = tf.Variable(tf.zeros([L3])/10)
-w4 = tf.Variable(tf.truncated_normal([L3,L4],stddev=0.1))
-b4 = tf.Variable(tf.zeros([L4])/10)
-w5 = tf.Variable(tf.truncated_normal([L4,10],stddev=0.1))
-b5 = tf.Variable(tf.zeros([10]))
 
 #the model
 
-xx=tf.reshape(x,[-1,784])
-y1=tf.nn.relu(tf.matmul(xx,w1)+b1)
-y1d=tf.nn.dropout(y1,pkeep)
+xx=tf.reshape(x,[-1,784],name='Input_layer')
 
-y2=tf.nn.relu(tf.matmul(y1d,w2)+b2)
-y2d=tf.nn.dropout(y2,pkeep)
+with tf.name_scope('Hidden_layer1'):
+    w1 = tf.Variable(tf.truncated_normal([784,L1],stddev=0.1),name="W")
+    b1 = tf.Variable(tf.zeros([L1])/10,name="B")
+    y1=tf.nn.relu(tf.matmul(xx,w1)+b1)
+    y1d=tf.nn.dropout(y1,pkeep)
 
-y3=tf.nn.relu(tf.matmul(y2d,w3)+b3)
-y3d=tf.nn.dropout(y3,pkeep)
+with tf.name_scope('Hidden_layer2'):
+    w2 = tf.Variable(tf.truncated_normal([L1,L2],stddev=0.1),name="W")
+    b2 = tf.Variable(tf.zeros([L2])/10,name="B")
+    y2=tf.nn.relu(tf.matmul(y1d,w2)+b2)
+    y2d=tf.nn.dropout(y2,pkeep)
 
-y4=tf.nn.relu(tf.matmul(y3d,w4)+b4)
-y4d=tf.nn.dropout(y4,pkeep)
+with tf.name_scope('Hidden_layer3'):
+    w3 = tf.Variable(tf.truncated_normal([L2,L3],stddev=0.1),name="W")
+    b3 = tf.Variable(tf.zeros([L3])/10,name="B")
+    y3=tf.nn.relu(tf.matmul(y2d,w3)+b3)
+    y3d=tf.nn.dropout(y3,pkeep)
 
-lin_pred=tf.matmul(y4d,w5)+b5
-y=tf.nn.softmax(lin_pred)
+with tf.name_scope('Hidden_layer4'):
+    w4 = tf.Variable(tf.truncated_normal([L3,L4],stddev=0.1),name="W")
+    b4 = tf.Variable(tf.zeros([L4])/10,name="B")
+    y4=tf.nn.relu(tf.matmul(y3d,w4)+b4)
+    y4d=tf.nn.dropout(y4,pkeep)
+
+with tf.name_scope('Hidden_layer5'):
+    w5 = tf.Variable(tf.truncated_normal([L4,10],stddev=0.1),name="W")
+    b5 = tf.Variable(tf.zeros([10]),name="B")
+    lin_pred=tf.matmul(y4d,w5)+b5
+
+with tf.name_scope('Output_layer'):
+    y=tf.nn.softmax(lin_pred)
 
 #loss
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=lin_pred, labels=y_)
-cross_entropy = tf.reduce_mean(cross_entropy)*100
+with tf.name_scope("loss"):
+    #As per documentation, this function takes linear_predictors and does
+    #softmax conversion internally for efficiency.
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=lin_pred, labels=y_)
+    cross_entropy = tf.reduce_mean(cross_entropy)*100
 
 # accuracy
-correct_pred = tf.equal(tf.argmax(y_,1),tf.argmax(y,1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred,tf.float32))
+with tf.name_scope("accuracy"):
+    correct_pred = tf.equal(tf.argmax(y_,1),tf.argmax(y,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred,tf.float32))
 
-train=tf.train.GradientDescentOptimizer(lr).minimize(cross_entropy)
+with tf.name_scope("train"):
+    train=tf.train.GradientDescentOptimizer(lr).minimize(cross_entropy)
 
 # initialize tensorflow artifacts
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
+
+#setting up the filewirters
+train_writer=tf.summary.FileWriter("/tmp/tboard/nnet/1")
+train_writer.add_graph(sess.graph)
 
 
 for i in range(10000):
